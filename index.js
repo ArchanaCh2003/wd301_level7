dff9c9e842d441a81a20e16f33fe45c3a5aa54ea
ArchanaCh2003 <98682999+ArchanaCh2003@users.noreply.github.com>
2023-03-06 23:27:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,28 +41,7 @@ let d = [{
 }]
 
 let obj = [{}];
-/*
-(async () => {
-    await client.connect();
-    const result = await client.query('select * from todo_table');
 
-    //const result = await client.query('insert into todo_table(title,duedate,completed)
-    //values($1,$2,$3)',[parameters]);
-    //console.log(result.rows);
-    console.log(result.rowCount);
-    for (let i = 0; i < result.rowCount; i++) {
-        console.log(result.rows[0]);
-        //let date = client.query('select duedate::date from todo_table where title=$1',[result.rows[i].title]);
-        //console.log(date);
-    }
-    obj = result.rows;
-    //let d = obj[0].duedate;
-    //console.log(new Date(d) < new Date());
-    //client.end();
-})();
-
-
-*/
 
 app.get('/', function (req, res) {
     res.send("success");
@@ -99,43 +78,6 @@ app.get('/start', function (req, res) {
 
     });
 });
-
-/*const result = client.query('select * from todo_table', () => {
-    let t1 = result.rows[0].title;
-    console.log(t1);
-})
-
-
-app.get('/start', function (req, res) {
-
-
-    res.render('home', {
-
-        title: 'Assignment',
-        duedate: '2023-03-05T18:30:00.000Z',
-        completed: false
-
-    });
-    res.sendFile(__dirname + '/views/home.ejs');
-
-})
-
-  */
-
-/*res.render('home', {
-    output: [{
-        title: 'Assignment',
-        duedate: '2023-03-05T18:30:00.000Z',
-        completed: false
-    },
-    {
-        title: 'Assignment',
-        duedate: '2023-03-05T18:30:00.000Z',
-        completed: false
-    }]
-    ,
-})*/
-
 
 
 
