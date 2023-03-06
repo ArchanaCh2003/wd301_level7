936b801f0e457c2fef211471e6fff90b423bd7a6
ArchanaCh2003 <98682999+ArchanaCh2003@users.noreply.github.com>
2023-03-06 23:24:36 +0530

    Update database.js

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const client = new Client({
     host: "localhost",
     port: 5432,
     user: "postgres",
-    password: "postgresql",
+    password: "",
     database: "todo_list"
 })
 //==================
